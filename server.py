1e65e4b7c2ffdfe70616876a8ae8f6d4d194b7cd
Brockcito <thebrock1520@gmail.com>
2024-07-24 01:53:10 +0000

    Mundo guardado 2024-07-24

--- server.py
+++ server.py
@@ -20,7 +20,7 @@
 
 
 
-# No toques nada de aquí para abajo, puedes dañarlo
+--------oooooooooooooo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
